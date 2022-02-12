7c8a9a5f41532d4d0e0fa58e9112e3324c9d3cf3
Jags23 <89766280+Jags23@users.noreply.github.com>
2022-02-12 11:00:47 +0530

    Create area_circle.py

---
+++ area_circle.py
@@ -0,0 +1,7 @@
+import math
+r=input('Enter the radius :')
+rad=float(r)
+area=math.pi *(rad**2)
+print('The required area of circle of radius '+r+' is',area)
+
+#This program intends to find the area of cicle,whose radius is given
