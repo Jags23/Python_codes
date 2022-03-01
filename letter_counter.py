8dce979ea3721e53a5f6b94c8dfba7f76ab440d3
Jags23 <89766280+Jags23@users.noreply.github.com>
2022-03-01 19:27:43 +0530

    Update letter_counter.py
    Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. Use dictionaries.
    
    Your Input and output should look something like this
    
    Input : Please enter a string Mississippi
    
    Output : i = 04 s = 04 p =02 m =01

--- letter_counter.py
+++ letter_counter.py
@@ -4,12 +4,13 @@ def most_frequent(string):
         if c not in d:
             d[c] = 1
         else:
-            d[c] = d[c] + 1
+            d[c] = d[c] + 1                # d stores the letters available in the word, with their respective count(unsorted)
 
-    lst=list(d.keys())
-    
-    for i in lst:
-        print(i, d[i])
+    lst=sorted(d.items(), key=lambda x:x[1], reverse=True) # lst stores the the dict as a 2d list which is sorted by value
+    d1=dict(lst)                        #d1 is a dict made up of lst
+    lst1=list(d1.keys())                 #lst1 only has the key values of d1
+    for i in lst1:
+        print(i, d1[i])
     
 
 
