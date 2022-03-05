78cc0794b858586d9d4dbe42fdc97597cc6b8d98
Jags23 <89766280+Jags23@users.noreply.github.com>
2022-03-05 12:31:35 +0530

    Create School_Admin.py
    a school administration program that allows user to store student's Name, Age , Contact number, email id into a csv file

---
+++ School_Admin.py
@@ -0,0 +1,31 @@
+import csv
+
+def write_to_file(lst):
+    with open('info1.csv','a') as csv_file:
+        writer=csv.writer(csv_file)
+        
+        if csv_file.tell()==0:
+            writer.writerow(['Name','Age','Contact','Email'])
+            
+        writer.writerow(lst)
+
+if __name__=='__main__':
+
+    count=1
+    cond=True
+    while (cond):
+        info=input('Enter student #{} information in the specified format--> (Name age contact email) :'.format(count))
+        lst=info.split(' ')
+        print("\nEntered information is -\nName:{}\nAge:{}\nContact number:{}\nEmail:{}".format(lst[0],lst[1],lst[2],lst[3]))
+        correct=input("Do you like to enter this information? :(Y/N)")
+        if correct=='Y':
+             write_to_file(lst)
+             cont=input('Proceed to enter information of other students? (Y/N)')
+             count=count+1
+             if cont=='Y':
+                cond=True
+             elif cont=='N':
+                cond=False
+                
+        elif correct=='N':
+            print("\nPlease enter the correct information")
