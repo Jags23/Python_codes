798739ed585a1f4445d3217cba77eb7358c0207f
Jagadeep T <89766280+Jags23@users.noreply.github.com>
2022-09-15 13:48:59 +0530

    Create repeated_strings.py
    qn : https://www.hackerrank.com/challenges/repeated-string/problem?h_r=internal-search

---
+++ repeated_strings.py
@@ -0,0 +1,44 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    l=len(s)
+    rem=n%l
+    mul=n//l
+    c=0
+    for i in s:
+        if i=='a':
+            c=c+1
+        else:
+            continue
+    c=c*mul
+    extra=(s[:rem])
+    for e in extra:
+        if e=='a':
+            c=c+1
+    return(c)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
